0da9ed2ffb90098a5515c607026b841ab74867f6
xing-6464 <xing_6464@foxmail.com>
2023-10-11 00:39:26 +0800

    setPropsForDom

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import ReactDOM from './react-dom'
 
 // const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(<div>{ele} Hello Simple React</div>)
-const helloReact = <div>Hello React</div>
+const helloReact = <div style={{ color: 'red' }}>Hello React</div>
 console.log(helloReact)
 ReactDOM.render(helloReact, document.getElementById('root'))
--- src/react-dom.js
+++ src/react-dom.js
@@ -28,8 +28,24 @@ function createDOM(VNode) {
     }
   }
 
-  // TODO: 处理属性值
+  setPropsForDOM(dom, props)
   return dom
+}
+
+function setPropsForDOM(dom, VNodeProps = {}) {
+  if (!dom) return
+  for (let key in VNodeProps) {
+    if (key === 'children') continue
+    if (/^on[A-Z].*/.test(key)) {
+      // TODO: 事件处理
+    } else if (key === 'style') {
+      Object.keys(VNodeProps[key]).forEach((styleName) => {
+        dom.style[styleName] = VNodeProps[key][styleName]
+      })
+    } else {
+      dom[key] = VNodeProps[key]
+    }
+  }
 }
 
 function mountArray(children, parent) {
